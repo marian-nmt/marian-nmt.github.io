72716726187dd68489264ee4ddc007631b921664
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-11-21 13:00:45 +0000

    Update commit-and-deploy.sh

--- commit-and-deploy.sh
+++ commit-and-deploy.sh
@@ -3,12 +3,12 @@
 make update-docs
 git commit -a -m 'Commit local changes to Jekyll sources'
 git up
-git push
+git push origin jekyll
 JEKYLL_ENV=production bundle exec jekyll build
 git checkout master
 rsync -ca _site/* .
 rm -rf _site
 git add *
 git commit -a -m 'Commit static pages'
-git push
+git push origin master
 git checkout jekyll

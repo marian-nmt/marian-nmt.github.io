837a9edbeafcdaa2e5e8fe1214ff0029ab0f21e3
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-08-14 17:21:48 +0100

    Update deployment script

--- commit-and-deploy.sh
+++ commit-and-deploy.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-#make update-docs
 git commit -a -m 'Commit local changes to Jekyll sources' || true
-git up
 git push origin jekyll
 JEKYLL_ENV=production bundle exec jekyll build
 git checkout master
 rsync -ca _site/* .
-rm -rf _site *~
+rm -rf _site Doxyfile.marian.in *~
 git add * || true
-git add docs/marian || true
 git commit -a -m 'Commit static pages'
 git push origin master
 git checkout jekyll

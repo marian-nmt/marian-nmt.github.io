9d03e8e8c1630438cbc083b763810b48991dbc94
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2021-02-23 17:19:46 +0000

    Add Sphinx docs/api/

---
+++ docs/api/_static/documentation_options.js
@@ -0,0 +1,11 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v1.10.1 2021-02-23',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
